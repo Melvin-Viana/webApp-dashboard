87f61b1009f9c7bb94ae50f0d0ef4b2a26662afd
Melvin Viana <mjviana057@gmail.com>
2019-05-15 18:30:11 -0700

    Setup markup for  traffic chart

--- js/addCharts.js
+++ js/addCharts.js
@@ -1,34 +1,35 @@
- var ctx = document.getElementById('lineChart').getContext('2d');
- var myChart = new Chart(ctx, {
-     type: 'line',
-     data: {
-         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-         datasets: [{
-             data: [500, 750, 1000, 1000, 2000, 2100],
-             pointBackgroundColor: "#fff",
-             borderWidth: 1,
-             lineTension: 0
-         }],
-     },
-     options: {
-         scales: {
-             yAxes: [{
-                 ticks: {
-                     max: 2500,
-                     stepSize: 500
-                 }
-             }]
-         },
-         legend: {
-             display: false
-         },
-         tooltips: {
-             callbacks: {
-                 label: function (tooltipItem) {
-                     return tooltipItem.yLabel;
-                 }
-             }
-         },
-         responsive: true,
-     }
- });
+ let ctx = document.getElementById('lineChart').getContext('2d');
+ const lineChartConfig ={
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            data: [500, 750, 1000, 1000, 2000, 2100],
+            pointBackgroundColor: "#fff",
+            borderWidth: 1,
+            lineTension: 0
+        }],
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 2500,
+                    stepSize: 500
+                }
+            }]
+        },
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
+        },
+        responsive: true,
+    }
+}
+ let myChart = new Chart(ctx,lineChartConfig );
