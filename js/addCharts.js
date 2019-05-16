805c927561c74230046e0cda4de7e65013667fde
Melvin Viana <mjviana057@gmail.com>
2019-05-15 23:38:22 -0700

    Styling for line graph

--- js/addCharts.js
+++ js/addCharts.js
@@ -1,21 +1,24 @@
  let ctx = document.getElementById('lineChart').getContext('2d');
+ Chart.defaults.global.defaultFontFamily="'Lato', sans-serif";
  const lineChartConfig ={
     type: 'line',
     data: {
-        labels: ['1', '2', '3', '4', '5', '6'],
+        labels: ['3/31 - 4/6 ', '4/7 - 4/13', '4/14 - 4/20', '4/21 - 4/27', '4/28 - 5/4', '5/5 - 5/11','5/12 - 5/18','5/19 - 5/25','5/26 - 6/1','6/2 - 6/8','6/9 - 6/15'],
         datasets: [{
-            data: [500, 750, 1000, 900, 700, 1150],
+            data: [500, 750, 1000, 900, 700, 1150,1250,1400,2000,2100,1400],
             pointBackgroundColor: "#fff",
             borderWidth: 3,
-            lineTension: 0
+            lineTension: 0,
+            backgroundColor:"rgba(91, 192, 190, 0.31)",
+            borderColor:"#5bc0be",
         }],
     },
     options: {
         scales: {
             yAxes: [{
                 ticks: {
-                    max: 1200,
-                    stepSize: 100
+                    max: 2500,
+                    stepSize: 500
                 }
             }]
         },
