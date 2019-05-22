9e3717e343344af58ebbef3ac7448b04fa461cf0
Melvin Viana <mjviana057@gmail.com>
2019-05-21 17:21:05 -0700

    Change color for hover item-option li

--- js/app.js
+++ js/app.js
@@ -85,8 +85,8 @@ document.addEventListener('DOMContentLoaded', () => {
             });
             if (localStorage.getItem('timezone') !== null) {
                 selectTimezone.selectedIndex = localStorage.getItem('timezone');
-            }else if(localStorage.getItem('timezone') === null){
-                selectTimezone.selectedIndex=0;
+            } else if (localStorage.getItem('timezone') === null) {
+                selectTimezone.selectedIndex = 0;
             }
         })
     }
@@ -103,22 +103,37 @@ document.addEventListener('DOMContentLoaded', () => {
             popup.classList.remove('show');
         }
     });
-    if(window.innerWidth>512){
-    if(window.pageYOffset<149){sideBar.style.position="static"}
-    if(window.pageYOffset>149){sideBar.style.position="fixed"}
-    }else{
-        if(window.pageYOffset<75){sideBar.style.position="static"}
-        if(window.pageYOffset>75){sideBar.style.position="fixed"}
-
+    if (window.innerWidth > 512) {
+        if (window.pageYOffset < 149) {
+            sideBar.style.position = "static"
+        }
+        if (window.pageYOffset > 149) {
+            sideBar.style.position = "fixed"
+        }
+    } else {
+        if (window.pageYOffset < 75) {
+            sideBar.style.position = "static"
+        }
+        if (window.pageYOffset > 75) {
+            sideBar.style.position = "fixed"
+        }
     }
-    window.onscroll=function(){
-        if(window.innerWidth<512){
-            if(window.pageYOffset<149){sideBar.style.position="static"}
-            if(window.pageYOffset>149){sideBar.style.position="fixed"}
-            } 
-        if(window.innerWidth>=512){
-                if(window.pageYOffset<75){sideBar.style.position="static"}
-                if(window.pageYOffset>75){sideBar.style.position="fixed"}
-            }  
+    window.onscroll = function () {
+        if (window.innerWidth < 512) {
+            if (window.pageYOffset < 149) {
+                sideBar.style.position = "static"
+            }
+            if (window.pageYOffset > 149) {
+                sideBar.style.position = "fixed"
+            }
+        }
+        if (window.innerWidth >= 512) {
+            if (window.pageYOffset < 75) {
+                sideBar.style.position = "static"
+            }
+            if (window.pageYOffset > 75) {
+                sideBar.style.position = "fixed"
+            }
+        }
     }
 });
